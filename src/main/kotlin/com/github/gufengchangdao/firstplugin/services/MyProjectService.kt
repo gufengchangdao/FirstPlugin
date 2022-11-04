1f7c7f4198311da10b32f3852f20d15e39638db4
GitHub Action <action@github.com>
2022-11-04 13:32:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/gufengchangdao/firstplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gufengchangdao/firstplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gufengchangdao.firstplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gufengchangdao.firstplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
